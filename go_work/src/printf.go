edd87b03d86eb3313dbd178a4cf17f3537a96e64
Prashant Nidgunde <prashant.nidgunde@gmail.com>
2016-07-17 07:51:04 +0530

    changes after learning git. Feeling really good to upload these changes after learning how to work with git. Cheers git status!

--- go_work/src/printf.go
+++ go_work/src/printf.go
@@ -6,9 +6,9 @@ import "fmt"
 
 func main() {
         fmt.Printf("hello, world\n")
-      var number1 int = 0;
-      var number2 float64 =  10;
-      number3 := 44;   // Node that its := and no need to use var and shit
+      var number1 int = 0
+      var number2 float64 =  10
+      number3 := 44   // Node that its := and no need to use var and shit
 
       fmt.Printf(number1,number2,number3);
 }
