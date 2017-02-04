ea57fdf867f269ea5648661ae7ac41f8396ec230
pashnidgunde <pashnidgunde@gmail.com>
2017-02-04 18:21:06 -0500

    some new code

--- cpp/multithreading/producer_consumer.cpp
+++ cpp/multithreading/producer_consumer.cpp
@@ -1,60 +1,59 @@
-/*
- * producer_consumer.cpp
- *
- *  Created on: 19-Jan-2017
- *      Author: rupa
- */
-
-#include <condition_variable>
 #include <iostream>
-#include <list>
-#include <mutex>
 #include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <list>
+#include <string>
 
-int shared_list[10];
+std::list<std::string> messages;
 std::mutex m;
-std::condition_variable c;
+std::condition_variable v;
 
-class Producer {
- public:
-  void operator()() { produce(); }
-
- private:
-  void produce() {
-    while (1) {
-      std::unique_lock<std::mutex>(m);
-      int random = rand();
-      std::cout << "Pushing :: " << random << std::endl;
-      shared_list[(rand() % 10)] = rand();
-      c.notify_one();
-    }
-  }
-};
-
-class Consumer {
- public:
-  void operator()() { consume(); }
-
- private:
-  void consume() {
-    while (1) {
-      if (shared_list.size()) {
-        std::unique_lock<std::mutex>(m);
-        c.wait(m);
-        std::cout << " Popping :: " << shared_list.front() << std::endl;
-        shared_list.pop_front();
+class Producer{
+  public:
+  void operator()()
+  {
+      std::string msg ("Message from producer :: ") ;
+      while (1)
+      {
+          std::unique_lock<std::mutex> guard(m);
+          messages.push_back(msg);
+          v.notify_one();
       }
-    }
   }
+
 };
 
-int main() {
-  Producer p;
-  Consumer c;
 
-  std::thread producer_thread(p);
-  std::thread consumer_thread(c);
 
-  producer_thread.join();
-  consumer_thread.join();
+class Consumer
+{
+  public:
+  void operator()()
+  {
+      while (1)
+      {
+          std::unique_lock<std::mutex> guard(m);
+          v.wait(guard);
+          std::cout << "Recieved from Producer :: " << messages.front();
+      }
+  }
+
+};
+
+
+
+
+int main()
+{
+
+    Producer p;
+    std::thread producer_thread (p);
+
+    Consumer c;
+    std::thread consumer_thread (c);
+
+    producer_thread.join();
+    consumer_thread.join();
+
 }
