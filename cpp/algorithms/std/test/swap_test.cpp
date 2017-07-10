b4d2045e662b96d6c5ee6558adbb039848583c9e
pashnidgunde <pashnidgunde@gmail.com>
2017-07-10 07:15:41 -0400

    to check if a single swap can sort an array

--- cpp/algorithms/std/test/swap_test.cpp
+++ cpp/algorithms/std/test/swap_test.cpp
@@ -1,36 +1,24 @@
 // Implement version of std::swap
 
 #include <iostream>
-
-// Note that the values are passed by ref.
-template < class T>
-void swap ( T& l, T& r)
-{
-    T x;
-    x = l;
-    l = r;
-    r = x;
-}
-
-
+#include "../../swap.hpp"
 
 int main()
 {
     int a = 10;
     int b = 20;
 
-
     int *p = &a;
     int *q = &b;
 
-    swap( a, b);
+    pn::algo::swap(a, b);
     std::cout << a << " :: " << b << std::endl;
     // values are passed and not pointers
-    swap( *p, *q);
-    std::cout << a << " :: " << b << std::endl;
+    //swap(*p, *q);
+    //std::cout << a << " :: " << b << std::endl;
 
     // standard swap does not swap values in case of pointers
-    std::swap(p,q);
-    std::cout << a << " :: " << b << std::endl;
+    //std::swap(p,q);
+    //std::cout << a << " :: " << b << std::endl;
     return 0;
 }
