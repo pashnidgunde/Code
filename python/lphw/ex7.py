0a33f9affabd8b711e3ba5ce9d78820de0f526b8
pashnidgunde <pashnidgunde@gmail.com>
2018-03-24 19:30:07 -0400

    Adding lphw

---
+++ python/lphw/ex7.py
@@ -0,0 +1,24 @@
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+print("." * 20)  # what'd that do?
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch end = ' ' at the end  
+# The end specifies the end character , by default end is "\n" i.e std::endl
+print(end1 + end2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
+# This outputs to Cheese Burger, if you remove end character it will print Cheese and Burger on seperate lines
+
