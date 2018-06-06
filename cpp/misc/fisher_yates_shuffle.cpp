fb76dee872b2497b47a829be1031db5f7eaecd27
pashnidgunde <pashnidgunde@gmail.com>
2018-06-05 22:00:42 -0400

    fisher yates shuffle problem

---
+++ cpp/misc/fisher_yates_shuffle.cpp
@@ -0,0 +1,13 @@
+// Program to shuffle randomly shuffle elements
+// Algo:
+//   for i from n-1 downto 1 do
+//	j = random integer such that 0 <= j <=i
+//	exchange a[i] and a[j]
+
+#include <stdio.h>
+
+main()
+{
+	// seed
+	//srand(time(NULL));
+}
