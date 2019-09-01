bff95c4d901fbc4d5f90dc64b4dbe9ed082de2b6
pashnidgunde <pashnidgunde@gmail.com>
2019-09-01 17:53:13 -0400

    features and multithreading

---
+++ cpp/misc/multithreading/concurrency_in_action/ch1/hello_world.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <thread>
+
+// thread function
+void helloFunction
+() { std::cout << "Hello from thread" << std::endl; }
+
+// thread functor
+class HelloFunctor {
+    public:
+      void operator()() {
+        std::cout << "Hello World from functor." << std::endl;
+      }
+};
+
+
+
+int main() { 
+    std::thread functThread(helloFunction);
+    functThread.join();
+
+    HelloFunctor functor;
+    std::thread functorThread(functor);
+    functorThread.join();
+
+
+    return 0;
+}
--- cpp/misc/multithreading/openmp/hello.cpp
+++ cpp/misc/multithreading/openmp/hello.cpp
@@ -6,8 +6,6 @@ int main() {
 auto x = 0;
 #pragma omp parallel
 
-
-
 std::cout << "x: " << x << std::endl;
 
 printf("Hello from thread %d, nthreads %d\n", omp_get_thread_num(), omp_get_num_threads());
