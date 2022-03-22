c2c7c629df0ad5ee28a26100303266eaef8e2a96
unknown <pashnidgunde@gmail.com>
2022-03-22 19:52:33 +0100

    Some more practice code

--- debug/debug2.cpp
+++ debug/debug2.cpp
@@ -1,89 +1,97 @@
+//https://leetcode.com/problems/maximum-width-of-binary-tree/
 #include <iostream>
+#include <cassert>
 #include <string>
-#include <vector>
-#include <cmath>
 #include <sstream>
+#include <vector>
+#include <stack>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+
+struct TreeNode {
+      int val;
+      TreeNode* left;
+      TreeNode* right;
+      bool visited = false;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
 
 using namespace std;
-
-int determinant(std::vector<std::vector<int>> matrix, int n) {
-   int det = 0;
-   std::vector<std::vector<int>> submatrix(n, std::vector<int>(n, 0));
-   if (n == 2)
-   return ((matrix[0][0] * matrix[1][1]) - (matrix[1][0] * matrix[0][1]));
-   else {
-      for (int x = 0; x < n; x++) {
-         int subi = 0;
-         for (int i = 1; i < n; i++) {
-            int subj = 0;
-            for (int j = 0; j < n; j++) {
-               if (j == x)
-               continue;
-               submatrix[subi][subj] = matrix[i][j];
-               subj++;
-            }
-            subi++;
-         }
-         det = det + (pow(-1, x) * matrix[0][x] * determinant( submatrix, n - 1 ));
-      }
-   }
-   return det;
-}
-
-string MatrixChallenge(string strArr[], int arrLength) {
-  
-  int pos = 0;
-  for (int i=0; i< arrLength; i++)
-  {
-      if (strArr[i] == "<>")
-      {
-        pos = i;
-        break;
-      }
-  }
-
-  std::vector<std::vector<int>> matrix(pos, std::vector<int>(pos,0));
-  //std::vector<std::vector<int>> matrix;
-  int index = 0;
-
-  for (int i=0; i<pos; i++)
-  {
-    std::vector<int> v;
-    for (int j=0; j<pos;j++)
+class Solution {
+public:
+    int heightOfTree(TreeNode *node) 
     {
-      matrix[i][j] = std::atoi(strArr[index].c_str());
-      index++;
+        if (node != nullptr) 
+        {
+            int hl = heightOfTree(node->left);
+            int hr = heightOfTree(node->right);
+
+            if (hl > hr) 
+            {
+                return hl + 1;
+            } 
+            else 
+            {
+                return hr + 1;
+            }
+        }
+        return 0;
     }
-    index++;
-  }
-  
 
-  // code goes here  
-  auto d = determinant(matrix, pos);
-  std::cout << "Determinant : " << d << std::endl;
-  
-  std::stringstream ss;
-  ss << d;
-  return ss.str();
+    int widthOfBinaryTree(TreeNode* root) {
+        std::vector<TreeNode*> nodes;
+        nodes.resize(std::pow(2,heightOfTree(root)) -1, nullptr);
+        nodes[0] = root;
+        for (int i = 0 ; i < nodes.size(); i++)
+        {
+            auto node = nodes[i];
+            if (node)
+            {
+                nodes[2*i+1] = node->left;
+                nodes[2*i+2] = node->right;
+            }
+        }
+        
+        int begin = 1;
+        auto end = 0;
+        int level = 0;
+        int max_max = 1;
+        int mid = 0;
+        int node_count = 0;
+        
+        while (begin < nodes.size())
+        {
+            int max = 0;
+            node_count = std::pow(2,level);
+            begin = begin + node_count - 1;
+            mid = begin + node_count / 2;
+            end = begin + node_count - 1;
+            
+            if (std::any_of(nodes.begin()+begin, nodes.begin()+mid+1, [](TreeNode* node) { return node != nullptr; } ))
+            {
+               max += node_count / 2; 
+            }
+            
+            if (std::any_of(nodes.begin()+mid, nodes.begin()+end+1, [](TreeNode* node) { return node != nullptr; } ))
+            {
+               max += node_count / 2; 
+            }
+            
+            max_max = std::max(max_max,max);
+        }
+        
+        return max_max;
+    }
+};
 
-}
-
-
-int main(void) { 
-   
-  // keep this function call here
-  {
-    string A[] = {"5","0","<>","0","5"};
-    int arrLength = sizeof(A) / sizeof(*A);
-    cout << MatrixChallenge(A, arrLength);
-  }
-  
-  {
-    string A[] = {"1","2","4","<>","2","1","1","<>","4","1","1"};
-    int arrLength = sizeof(A) / sizeof(*A);
-    cout << MatrixChallenge(A, arrLength);
-  }
-
-  return 0;
+int main()
+{
+    Solution s;
+    TreeNode *root = new TreeNode(1,new TreeNode(3,new TreeNode(5), new TreeNode(3)),new TreeNode(2, nullptr, new TreeNode(9)));
+    s.widthOfBinaryTree(root);
     
+    return 0;
 }
